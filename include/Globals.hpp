c49ddafb88042a3621d9b85d2e6bf6f79985fa38
mmeshcher <keyvrn12012014@icloud.com>
2020-05-29 23:43:46 +0300

    f

--- include/Globals.hpp
+++ include/Globals.hpp
@@ -9,7 +9,7 @@ struct Globals
     static std::string output;
     static std::string input;
     static bool writeOnly;
-    static const size_t BAREV = 1;
+};
+  static const size_t BAREV = 1;
 static const size_t CHKORES = 5;
 static const size_t MINCH = 25;
-};
